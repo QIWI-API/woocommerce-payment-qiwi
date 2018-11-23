5ecc67a72111637edc2a199b15dbf338fd10e3ed
Yaroslav <yaroslav@wannabe.pro>
2018-11-23 13:46:54 +0500

    Append validate plugin installation message.

--- woocommerce-payment-qiwi.php
+++ woocommerce-payment-qiwi.php
@@ -19,29 +19,54 @@ defined( 'ABSPATH' ) || exit;
 
 // On Woocommerce ready.
 add_action( 'woocommerce_init', function () {
-	// Autoload.
-	if ( file_exists( __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php' ) ) {
-		include_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
-	} else {
-		include_once __DIR__ . DIRECTORY_SEPARATOR . 'includes' . DIRECTORY_SEPARATOR . 'class-gateway.php';
-	}
-
 	// Translations.
 	load_plugin_textdomain( 'woocommerce_payment_qiwi', false, basename( __DIR__ ) . DIRECTORY_SEPARATOR . 'languages' );
 
-	// Add menu link.
-	add_action( 'admin_menu', function () {
-		/*
-		 * translators:
-		 * ru_RU: QIWI Касса
-		 */
-		$title = __( 'QIWI cash', 'woocommerce_payment_qiwi' );
-		add_submenu_page( 'woocommerce', $title, $title, 'manage_woocommerce', 'admin.php?page=wc-settings&tab=checkout&section=qiwi' );
-	}, 51 );
+	// Composer autoload.
+	if ( file_exists( __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php' ) ) {
+		include_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+	}
+	// if it's composer wordpress instalations.
+	else if ( class_exists( 'Qiwi\Api\BillPayments' ) ) {
+		include_once __DIR__ . DIRECTORY_SEPARATOR . 'includes' . DIRECTORY_SEPARATOR . 'class-gateway.php';
+	}
 
-	// Add gateway.
-	add_filter( 'woocommerce_payment_gateways', function ( $methods ) {
-		$methods[] = \Qiwi\Payment\Gateway::class;
-		return $methods;
-	} );
+	// Ready for use.
+	if ( class_exists( 'Qiwi\Payment\Gateway' ) )
+	{
+		// Add menu link.
+		add_action( 'admin_menu', function () {
+			/*
+             * translators:
+             * ru_RU: QIWI Касса
+             */
+			$title = __( 'QIWI cash', 'woocommerce_payment_qiwi' );
+			add_submenu_page( 'woocommerce', $title, $title, 'manage_woocommerce', 'admin.php?page=wc-settings&tab=checkout&section=qiwi' );
+		}, 51 );
+
+		// Add gateway.
+		add_filter( 'woocommerce_payment_gateways', function ( $methods ) {
+			$methods[] = \Qiwi\Payment\Gateway::class;
+			return $methods;
+		} );
+	}
+	// Broken installation.
+	else {
+		add_action( 'admin_notices', function () {
+			/** @var array $data Plugin data. */
+			$data = get_plugin_data( __FILE__, true, true );
+			/*
+             * translators:
+             * ru_RU: <div class="error"><p><strong>Ошибка:</strong> плагин «%1$s» установлен неправильно.</p><p>Пожалуйста, ознакомьтесь с инструкциями по <a href="%2$s" target="_blank">установке в ручную</a>.</p></div>
+			 * %1$s: Localised plugin name
+			 * %2$s: URL to instalation manual
+             */
+			printf(
+				__( '<div class="error"><p><strong>Error:</strong> Plugin "%1$s" is not installed correctly.</p><p>Please, see instructions for <a href="%2$s" target="_blank">manual instalation</a>.</p></div>', 'woocommerce_payment_qiwi' ),
+				$data['Name'],
+				$data['PluginURI'] . '#manual-installation'
+			);
+		} );
+	}
+
 } );
